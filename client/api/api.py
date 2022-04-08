87420f525a92d8684bb2565f5288b8ed00a6e367
Alani <AlaniW@Alanis-Air.attlocal.net>
2022-04-08 12:07:08 -0400

    search functionality :poop:

--- client/api/api.py
+++ client/api/api.py
@@ -1,13 +1,24 @@
+import flask
 from flask import Flask
+
+from imdb import search_movies
 
 app = Flask(__name__)
 
 # API Route
 @app.route("/endpoint")
 def functionname():
+    movie_results = search_movies("narnia")
+    print(movie_results)
     # do stuff
-    return {"names": ["Name1", "Name2", "Name3"]}
+    # return movie_results
 
+
+# @app.route("/query", methods=["POST"])
+# def query():
+#     data = flask.request.json()
+#     return data
+functionname()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
 
 function App() {
   const [data, setData] = useState({})
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:3000/endpoint").then(
@@ -14,6 +16,26 @@ function App() {
       }
     )
   }, [])
+
+  function handleChange(e) {
+    setSearch(e.target.value)
+    console.log(search)
+  }
+
+  function sendQuery() {
+    console.log(search)
+    //   fetch('/query', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(search),
+    //   })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       console.log(data);
+    //     });
+  }
 
   return (
     <div>
@@ -34,6 +56,9 @@ function App() {
         <Link to="/home">Home</Link> |{" "}
         <Link to="/mylist">My List</Link> |{" "}
         <Link to="/profile">Profile</Link>
+        <input type="text" placeholder="Search" onChange={handleChange}></input>
+        <button type="button" onClick={() => sendQuery()}><IoIosSearch /></button>
+
       </nav>
       {(typeof data.names === 'undefined') ? (
         <p>Loading...</p>
