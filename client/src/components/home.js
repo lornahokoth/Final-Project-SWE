fd68e4b455cdf50705f545046549d22d16599ad7
Alani <AlaniW@Alanis-Air.attlocal.net>
2022-04-21 14:06:37 -0400

    created trending page so that home page can be used for landing page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -9,43 +9,7 @@ import Searchbar from '../SearchBar';
 import './components.css';
 
 export default function Home() {
-    const [content, setContent] = useState([]);
-    const [movies, setMovies] = useState([]);
-    const [books, setBooks] = useState([]);
-    // const navigate = useNavigate()
 
-    // function search(query) {
-    //     const urlEncodedQuery = encodeURI(query);
-    //     navigate('/search');
-    // }
-
-
-    useEffect(() => {
-        fetch("/trending")
-            .then((res) => res.json())
-            .then(data => {
-                console.log(data);
-                setContent(data.results);
-            })
-    }, [])
-
-    useEffect(() => {
-        fetch("/trendingMovies")
-            .then((res) => res.json())
-            .then(data => {
-                console.log(data);
-                setMovies(data.items);
-            })
-    }, [])
-
-    useEffect(() => {
-        fetch("/bestSellers")
-            .then((res) => res.json())
-            .then(data => {
-                console.log(data);
-                setBooks(data.results.books);
-            })
-    }, [])
 
     return (
         <main className="main" style={{ padding: "1rem 0" }}>
@@ -70,27 +34,13 @@ export default function Home() {
                 }}
             >
                 <Link to="/home">Home</Link> |{" "}
+                <Link to="/trending_page">Trending</Link> |{" "}
                 <Link to="/mylist">My Lists</Link> |{" "}
                 <Link to="/profile">Profile</Link> |{" "}
                 <Link to="/contactForm">Contact Form</Link> |{" "}
                 <Link to="/search">Search</Link>
                 {/* <Searchbar search={search} /> */}
             </nav >
-            <div>
-                <p className="pageTitle">Trending</p>
-            </div>
-            <div className="trending">
-                {movies.map((movieReq) =>
-                    <Movie key={movieReq.id} {...movieReq} />)}
-            </div>
-            <div className="trending">
-                {content.map((tvReq) =>
-                    <Tv key={tvReq.id} {...tvReq} />)}
-            </div>
-            <div className="trending">
-                {books.map((bookReq) =>
-                    <Books key={bookReq.rank} {...bookReq} />)}
-            </div>
         </main >
     );
 }
